8f78732e04536243f287563bc324e2054d0ea2c9
Miguel <miguelpiresmelo@gmail.com>
2025-09-19 11:47:05 -0300

    css

--- gerenciador_web/config/config/settings.py
+++ gerenciador_web/config/config/settings.py
@@ -115,7 +115,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'tarefas/static',  # Aqui você define onde está a pasta 'static'
+]
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- gerenciador_web/config/tarefas/views.py
+++ gerenciador_web/config/tarefas/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
 from .models import Tarefa
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
 # Create your views here.
 
+# def listar_tarefas(request):
+
+#     tarefas_salvas = Tarefa.objects.all()
+#     print(tarefas_salvas)
+#     return HttpResponse("View 'listar_tarefas' executada! Confira no terminal")
+
 def listar_tarefas(request):
+    # 1. A busca no banco de dados continua a mesma
     tarefas_salvas = Tarefa.objects.all()
-    print(tarefas_salvas)
-    return HttpResponse("View 'listar_tarefas' executada! Confira no terminal")
+
+    # 2. Criamos um "dicionario de contexto para enviar os dados ao template"
+    # A chave 'minhas_tarefas' será a variavel que usaram HTML.
+    contexto = {
+        'minhas_tarefas': tarefas_salvas
+    }
+
+    # 3. renderizamos o template, passando a requisição e o contexto com os dados.
+    return render(request, 'tarefas/lista.html', contexto)
