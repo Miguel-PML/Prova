1da154eadb57db5fcb7e2e7058885d4a65474455
Miguel <miguelpiresmelo@gmail.com>
2025-10-09 10:29:07 -0300

    sla

--- gerenciador_web/config/tarefas/urls.py
+++ gerenciador_web/config/tarefas/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views # importa nossas views 
 
 urlpatterns = [
-    path('', views.listar_tarefas, name = 'lista_tarefas') #seria /tarefas/
+    path('', views.listar_tarefas, name = 'lista_tarefas'), #seria /tarefas/
+    path('<int:tarefa_id>/', views.detalhe_tarefa, name='detalhe_tarefa'),
+    #alterar tarefa
+    path('<int:tarefa_id>/alterar/', views.alterar_tarefa, name = 'alterar_tarefa')
 ]
--- gerenciador_web/config/tarefas/views.py
+++ gerenciador_web/config/tarefas/views.py
@@ -1,24 +1,82 @@
-from django.shortcuts import render
-from .models import Tarefa
-#from django.http import HttpResponse
+from django.shortcuts import render,redirect
 
 # Create your views here.
 
-# def listar_tarefas(request):
-
-#     tarefas_salvas = Tarefa.objects.all()
-#     print(tarefas_salvas)
-#     return HttpResponse("View 'listar_tarefas' executada! Confira no terminal")
+from .models import Tarefa
+from django.http import HttpResponse
 
 def listar_tarefas(request):
-    # 1. A busca no banco de dados continua a mesma
+    #1 a busca no banco de dados continua a mesma
     tarefas_salvas = Tarefa.objects.all()
-
-    # 2. Criamos um "dicionario de contexto para enviar os dados ao template"
-    # A chave 'minhas_tarefas' será a variavel que usaram HTML.
+    #2 criamos um  dicionario de contexto para enviar os dados ao template
+    #a chave 'minhas_tarefas',será a variavel que usamos no html 
     contexto = {
         'minhas_tarefas': tarefas_salvas
     }
 
-    # 3. renderizamos o template, passando a requisição e o contexto com os dados.
+from django.shortcuts import render, get_object_or_404
+from .models import Tarefa
+
+def listar_tarefas(request):
+    # 1. a busca no banco de dados continua a mesma
+    tarefas_salvas = Tarefa.objects.all()
+
+    # 2. criamos um "dicionario do contexto" para enviar os dados ao template.
+    # A chave 'minhas_tarefas' será a variável que usaremos no html.
+    contexto = {
+        'minhas_tarefas': tarefas_salvas 
+    }
+
+    # 3. renderizamos o tamplate, passando a requisição e o contexto com os dados.
     return render(request, 'tarefas/lista.html', contexto)
+
+def detalhe_tarefa (request, tarefa_id):
+    #Busca uma tarefa pelo id
+    #Se não encontrar retorna um erro 404
+    tarefa= get_object_or_404 (Tarefa, pk=tarefa_id)
+
+    return render(request, 'tarefas/detalhe.html', {'tarefa':tarefa})
+
+
+
+#3. renderizamos o template , passando a requesição e o contexto com os dados. 
+    return render(request, 'tarefas/lista.html', contexto)
+
+def adicionar_tarefa(request):
+    if request.method == 'POST':
+        titulo = request.POST.get('titulo')
+        descricao = request.POST.get('descricao')
+        Tarefa.objects.create(titulo=titulo,descricao=descricao)
+        return redirect('lista_tarefas')
+
+    return render(request,'tarefas/form_tarefa.html')
+
+#me fodos http
+#POST: envia dados para o servidor
+#GET: busca dados no servidor 
+#PUT: atualizada recursos existentes
+#DELETES: remove recursos selecionados
+
+
+def alterar_tarefa(request, tarefa_id):
+    tarefa = get_object_or_404(Tarefa, pk=tarefa_id)
+    
+    if request.method == 'POST':
+        titulo = request.POST.get('titulo')
+        descricao = request.POST.get('descricao')
+        projeto_id = request.POST.get('projeto')
+        concluida = request.POST.get('concluido') == 'on' 
+
+      
+        tarefa.titulo = titulo
+        tarefa.descricao = descricao
+        tarefa.concluido = concluida
+        
+        tarefa.save()
+        
+        return redirect('lista_tarefas')
+
+    context = {
+        'tarefa': tarefa,
+    }
+    return render(request, 'tarefas/form_tarefa.html', context)
