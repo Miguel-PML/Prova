885b9f33e67bf16d7ccf7d0b1375098810527328
Miguel <miguelpiresmelo@gmail.com>
2025-10-10 11:57:09 -0300

    muita coisa

--- gerenciador_web/config/tarefas/urls.py
+++ gerenciador_web/config/tarefas/urls.py
@@ -5,5 +5,10 @@ urlpatterns = [
     path('', views.listar_tarefas, name = 'lista_tarefas'), #seria /tarefas/
     path('<int:tarefa_id>/', views.detalhe_tarefa, name='detalhe_tarefa'),
     #alterar tarefa
-    path('<int:tarefa_id>/alterar/', views.alterar_tarefa, name = 'alterar_tarefa')
+    path('<int:tarefa_id>/alterar/', views.alterar_tarefa, name = 'alterar_tarefa'),
+    #excluir tarefa
+    path('<int:tarefa_id>/excluir/', views.excluir_tarefa, name = 'excluir_tarefa'),
+     #Adicionar tarefas
+    path('adicionar/', views.adicionar_tarefa, name='adicionar_tarefa')
 ]
+
